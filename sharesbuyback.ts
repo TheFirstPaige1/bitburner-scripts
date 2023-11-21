08412f62608138c94b76986fb7578f811d5bd6fe
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-22 04:46:14 +1030

    my idiot ass forgot to formatNumber

--- sharesbuyback.ts
+++ sharesbuyback.ts
@@ -3,7 +3,7 @@ export async function main(ns: NS): Promise<void> {
 	ns.disableLog('ALL');
 	while (ns.corporation.getCorporation().issuedShares > 0) {
 		let sharestobuy = Math.trunc((ns.getServerMoneyAvailable("home") / ns.corporation.getCorporation().sharePrice) * 0.89);
-		ns.print("buying " + sharestobuy + " shares");
+		ns.print("buying " + ns.formatNumber(sharestobuy) + " shares");
 		ns.corporation.buyBackShares(sharestobuy);
 		await ns.sleep(60000);
 	}
