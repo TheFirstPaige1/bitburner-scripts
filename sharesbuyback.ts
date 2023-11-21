0d0ff655c2c43332d1e3014822e10f792970b611
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-22 06:36:09 +1030

    fixed an error when low on issued shares

--- sharesbuyback.ts
+++ sharesbuyback.ts
@@ -3,6 +3,7 @@ export async function main(ns: NS): Promise<void> {
 	ns.disableLog('ALL');
 	while (ns.corporation.getCorporation().issuedShares > 0) {
 		let sharestobuy = Math.trunc((ns.getServerMoneyAvailable("home") / ns.corporation.getCorporation().sharePrice) * 0.89);
+		sharestobuy = Math.min(sharestobuy, ns.corporation.getCorporation().issuedShares);
 		ns.print("buying " + ns.formatNumber(sharestobuy) + " shares");
 		ns.corporation.buyBackShares(sharestobuy);
 		ns.print(ns.formatNumber(ns.corporation.getCorporation().issuedShares) + " shares left to buy");
