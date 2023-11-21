3f7e2b11804a7d4fbd736887746ee0595bbf12ca
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-22 05:00:55 +1030

    cannot help but want to tinker it seems

--- sharesbuyback.ts
+++ sharesbuyback.ts
@@ -4,6 +4,7 @@ export async function main(ns: NS): Promise<void> {
 	while (ns.corporation.getCorporation().issuedShares > 0) {
 		let sharestobuy = Math.trunc((ns.getServerMoneyAvailable("home") / ns.corporation.getCorporation().sharePrice) * 0.89);
 		ns.print("buying " + ns.formatNumber(sharestobuy) + " shares");
+		ns.print(ns.formatNumber(ns.corporation.getCorporation().issuedShares) + " shares left to buy");
 		ns.corporation.buyBackShares(sharestobuy);
 		await ns.sleep(60000);
 	}
