52726acf5dd6e03a6ef922824a1472c86563f402
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-22 05:04:09 +1030

    woops

--- sharesbuyback.ts
+++ sharesbuyback.ts
@@ -4,8 +4,8 @@ export async function main(ns: NS): Promise<void> {
 	while (ns.corporation.getCorporation().issuedShares > 0) {
 		let sharestobuy = Math.trunc((ns.getServerMoneyAvailable("home") / ns.corporation.getCorporation().sharePrice) * 0.89);
 		ns.print("buying " + ns.formatNumber(sharestobuy) + " shares");
-		ns.print(ns.formatNumber(ns.corporation.getCorporation().issuedShares) + " shares left to buy");
 		ns.corporation.buyBackShares(sharestobuy);
+		ns.print(ns.formatNumber(ns.corporation.getCorporation().issuedShares) + " shares left to buy");
 		await ns.sleep(60000);
 	}
 }
