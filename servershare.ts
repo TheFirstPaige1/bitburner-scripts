6a618c67ab925680d444c1fd2e34bfffb60be7a3
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-22 22:05:27 +1030

    basic private server sharing

---
+++ servershare.ts
@@ -0,0 +1,13 @@
+import { NS } from "@ns";
+export async function main(ns: NS): Promise<void> {
+	const pservs = ns.getPurchasedServers();
+	if (pservs.length > 0) {
+		for (const server of pservs) {
+			ns.killall(server);
+			ns.scp("manshare.js", server);
+			if (ns.getServerMaxRam(server) >= 4) {
+				ns.exec("manshare.js", server, Math.trunc(ns.getServerMaxRam(server) / 8));
+			}
+		}
+	}
+}
