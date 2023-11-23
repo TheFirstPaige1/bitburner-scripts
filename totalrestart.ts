702ddfbe27498e6f81beeedb73b9af58d4a1e6c2
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-24 02:55:45 +1030

    totalrestart now also shares half of home's ram

--- totalrestart.ts
+++ totalrestart.ts
@@ -1,5 +1,7 @@
 import { NS } from "@ns";
 export async function main(ns: NS): Promise<void> {
 	ns.scriptKill("totalhack.js", "home");
+	ns.scriptKill("manshare.js", "home");
 	ns.run("totalhack.js");
+	ns.run("manshare.js", Math.trunc(ns.getServerMaxRam("home") / 8));
 }
