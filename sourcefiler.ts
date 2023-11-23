08349693bff8118daa3590a7dec2c32add416e1f
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-23 19:45:03 +1030

    formatting

--- sourcefiler.ts
+++ sourcefiler.ts
@@ -4,9 +4,7 @@ export async function main(ns: NS): Promise<void> {
 	const currentnode = ns.getResetInfo().currentNode;
 	let sortedfiles = [];
 	for (let i = 0; i < fetchedfiles.length; i++) { sortedfiles.push(0); }
-	for (const fileobject of fetchedfiles) {
-		sortedfiles[fileobject.n] = fileobject.lvl;
-	}
+	for (const fileobject of fetchedfiles) { sortedfiles[fileobject.n] = fileobject.lvl; }
 	sortedfiles[0] = currentnode;
 	ns.rm("sourcefiles.txt");
 	ns.write("sourcefiles.txt", sortedfiles.toString());
