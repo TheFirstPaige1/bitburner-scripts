cded1881c939df28247a88d4da34cc5265df2110
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-24 03:41:47 +1030

    expanded h4ckrnet to handle cache and sell hashes

--- h4ckrnet.ts
+++ h4ckrnet.ts
@@ -3,11 +3,19 @@ export async function main(ns: NS): Promise<void> {
 	ns.disableLog('ALL');
 	ns.tail();
 	let running = true;
+	let hackservs = false;
+	if (ns.fileExists("sourcefiles.txt", "home")) {
+		let bitnodes = ns.read("sourcefiles.txt").split(",");
+		if (bitnodes[0] == "9" || (bitnodes.length > 8 && bitnodes[9] != "")) {
+			hackservs = true;
+		}
+	}
 	while (running == true) {
 		let cdex = 0;
 		let clevelnode = 0;
 		let cramnode = 0;
 		let ccorenode = 0;
+		let chashnode = 0;
 		let ccost = Infinity;
 		if (ns.hacknet.numNodes() < ns.hacknet.maxNumNodes()) {
 			ccost = ns.hacknet.getPurchaseNodeCost();
@@ -34,31 +42,45 @@ export async function main(ns: NS): Promise<void> {
 				cdex = 3;
 			}
 		}
+		if (hackservs) {
+			for (let i = 0; i < ns.hacknet.numNodes(); i++) {
+				if (ns.hacknet.getCacheUpgradeCost(i) < ccost) {
+					ccost = ns.hacknet.getCacheUpgradeCost(i);
+					chashnode = i;
+					cdex = 4;
+				}
+			}
+		}
 		ns.print("Next cost is $" + ns.formatNumber(ccost));
 		while (ns.getServerMoneyAvailable("home") < ccost) {
+			if (hackservs && ns.hacknet.numHashes() > ns.hacknet.hashCost("Sell for Money")) {
+				ns.hacknet.spendHashes("Sell for Money");
+			}
 			await ns.sleep(1000);
 		}
 		switch (cdex) {
 			case 0:
 				ns.hacknet.purchaseNode();
-				ns.print("Buying Hacknet Node at cost $" + ns.formatNumber(ccost));
+				ns.print("Expanding Hacknet at cost $" + ns.formatNumber(ccost));
 				break;
 			case 1:
 				ns.hacknet.upgradeLevel(clevelnode);
-				ns.print("Upgrading Hacknet Node " + clevelnode + " level");
+				ns.print("Upgrading " + ns.hacknet.getNodeStats(clevelnode).name + " level");
 				break;
 			case 2:
 				ns.hacknet.upgradeRam(cramnode);
-				ns.print("Upgrading Hacknet Node " + cramnode + " RAM");
+				ns.print("Upgrading " + ns.hacknet.getNodeStats(clevelnode).name + " RAM");
 				break;
 			case 3:
 				ns.hacknet.upgradeCore(ccorenode);
-				ns.print("Upgrading Hacknet Node " + ccorenode + " core");
+				ns.print("Upgrading " + ns.hacknet.getNodeStats(clevelnode).name + " core");
 				break;
+			case 4:
+				ns.hacknet.upgradeCache(chashnode);
+				ns.print("Upgrading " + ns.hacknet.getNodeStats(clevelnode).name + " cache");
 		}
 		if (ccost == Infinity) {
 			running = false;
 		}
 	}
-	ns.closeTail();
 }
