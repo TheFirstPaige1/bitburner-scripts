47199a4fe74b1d5b0e28d3e926eb9ebeba508fc6
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-23 19:43:22 +1030

    sourcefiler now outputs a more useful array

--- sourcefiler.ts
+++ sourcefiler.ts
@@ -5,13 +5,9 @@ export async function main(ns: NS): Promise<void> {
 	let sortedfiles = [];
 	for (let i = 0; i < fetchedfiles.length; i++) { sortedfiles.push(0); }
 	for (const fileobject of fetchedfiles) {
-		if (fileobject.n == currentnode) {
-			sortedfiles[fileobject.n] = fileobject.n + "." + (fileobject.lvl + 1);
-		} else {
-			sortedfiles[fileobject.n] = fileobject.n + "." + fileobject.lvl;
-		}
+		sortedfiles[fileobject.n] = fileobject.lvl;
 	}
-	sortedfiles = sortedfiles.slice(1);
+	sortedfiles[0] = currentnode;
 	ns.rm("sourcefiles.txt");
 	ns.write("sourcefiles.txt", sortedfiles.toString());
 }
