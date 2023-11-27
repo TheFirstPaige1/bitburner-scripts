338eea287c9368362cab1367488ece485fe5ca1d
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-27 19:58:38 +1030

    made connecter output easier to read

--- connecter.ts
+++ connecter.ts
@@ -7,8 +7,13 @@ export async function main(ns: NS): Promise<void> {
 	for (let i = networkmap.length - 1; i > 0; i--) {
 		if (networkmap[i][0] == next) {
 			next = networkmap[i][1];
-			netpath.push(networkmap[i][1]);
+			netpath.unshift(networkmap[i][1]);
 		}
 	}
-	ns.tprint(netpath);
+	if (netpath.length > 10) {
+		ns.tprint(netpath.slice(0, 9));
+		ns.tprint(netpath.slice(10));
+	} else {
+		ns.tprint(netpath);
+	}
 }
