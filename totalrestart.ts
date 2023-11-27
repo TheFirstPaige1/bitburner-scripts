ae2223b055733d63c61f3a69c886ee59f48cdac2
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-28 00:43:13 +1030

    expanded totalrestart a little

--- totalrestart.ts
+++ totalrestart.ts
@@ -2,6 +2,14 @@ import { NS } from "@ns";
 export async function main(ns: NS): Promise<void> {
 	ns.scriptKill("totalhack.js", "home");
 	ns.scriptKill("manshare.js", "home");
+	if (!ns.fileExists("sourcefiles.txt", "home")) {
+		ns.run("sourcefiler.js");
+		await ns.sleep(10);
+	}
+	if (!ns.fileExists("networkmap.txt", "home")) {
+		ns.run("netscanner.js");
+		await ns.sleep(10);
+	}
 	ns.run("totalhack.js");
 	ns.run("manshare.js", Math.trunc(ns.getServerMaxRam("home") / 8));
 }
