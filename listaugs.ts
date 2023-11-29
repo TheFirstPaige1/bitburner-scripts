a10b90b6b64839ffd78b1a04be415d8c499ae7c8
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-11-29 11:23:13 +1030

    listaugs sorts high>low rep cost now

--- listaugs.ts
+++ listaugs.ts
@@ -1,27 +1,41 @@
 import { NS } from "@ns";
 import { Multipliers } from "@ns";
 export async function main(ns: NS): Promise<void> {
+	const combat = ns.args[0];
 	const desiredstats: (keyof Multipliers)[] = ["charisma", "charisma_exp", "company_rep", "faction_rep", "hacking", "hacking_chance",
 		"hacking_exp", "hacking_grow", "hacking_money", "hacking_speed", "hacknet_node_money"];
-	const desiredaugs = ["CashRoot Starter Kit", "Neuroreceptor Management Implant", "The Blade's Simulacrum", "The Red Pill"];
+	const desiredaugs = ["CashRoot Starter Kit", "Neuroreceptor Management Implant", "The Red Pill"]; //"The Blade's Simulacrum", not needed for now
 	const factions = ns.getPlayer().factions;
 	const playeraugs = ns.singularity.getOwnedAugmentations(true);
-	let auglist = [] as Array<string>;
+	let auglist = [];
 	for (const aug of desiredaugs) { if (!playeraugs.includes(aug)) { auglist.push(aug); } }
 	for (const faction of factions) {
 		const factaugs = ns.singularity.getAugmentationsFromFaction(faction);
 		for (const targaug of factaugs) {
 			if (!auglist.includes(targaug) && !playeraugs.includes(targaug)) {
-				for (const stat of desiredstats) {
+				if (combat) { if (!auglist.includes(targaug)) { auglist.push(targaug); } }
+				else {
 					let augstats = ns.singularity.getAugmentationStats(targaug);
-					if (augstats[stat] > 1 && !auglist.includes(targaug)) { auglist.push(targaug); }
+					for (const stat of desiredstats) { if (augstats[stat] > 1 && !auglist.includes(targaug)) { auglist.push(targaug); } }
 				}
 			}
 		}
 	}
-	ns.tprint(auglist.toString());
+	let sortedlist = [];
+	while (auglist.length > 0) {
+		let highdex = 0;
+		let highrep = 0;
+		for (let i = 0; i < auglist.length; i++) {
+			if (ns.singularity.getAugmentationRepReq(auglist[i]) > highrep) {
+				highrep = ns.singularity.getAugmentationRepReq(auglist[i]);
+				highdex = i;
+			}
+		}
+		sortedlist.push(auglist[highdex]);
+		auglist.splice(highdex);
+	}
+	ns.tprint(sortedlist.toString());
 	//TODO:
-	//sort by reputation cost high -> low
 	//check against a list of factions ordered early -> late
 	//work for each in turn until all augs are acquired
 }
