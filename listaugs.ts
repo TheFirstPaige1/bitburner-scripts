618d37423aad38508f29755876d75fae70f62129
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-12-01 23:03:11 +1030

    listaugs now actually lists augs and also faction

--- listaugs.ts
+++ listaugs.ts
@@ -7,8 +7,12 @@ export async function main(ns: NS): Promise<void> {
 	const desiredstats: (keyof Multipliers)[] = ["charisma", "charisma_exp", "company_rep", "faction_rep", "hacking", "hacking_chance",
 		"hacking_exp", "hacking_grow", "hacking_money", "hacking_speed", "hacknet_node_money"];
 	const desiredaugs = ["CashRoot Starter Kit", "Neuroreceptor Management Implant", "The Red Pill"]; //"The Blade's Simulacrum", not needed for now
-	const factions = ns.getPlayer().factions;
+	const desiredfactions = ["Netburners", "Tian Di Hui", "Aevum", "Daedalus", "CyberSec", "NiteSec", "Tetrads", "Bachman & Associates", "BitRunners", "ECorp",
+		"Fulcrum Secret Technologies", "Four Sigma", "The Black Hand", "The Dark Army", "Clarke Incorporated", "OmniTek Incorporated", "NWO", "Chongqing",
+		"Blade Industries", "MegaCorp", "KuaiGong International", "Slum Snakes", "Speakers for the Dead", "The Syndicate", "The Covenant"];
 	const playeraugs = ns.singularity.getOwnedAugmentations(true);
+	let factions = ns.getPlayer().factions;
+	for (const fac of desiredfactions) { if (!factions.includes(fac)) { factions.push(fac); } }
 	let auglist = [];
 	for (const aug of desiredaugs) { if (!playeraugs.includes(aug)) { auglist.push(aug); } }
 	for (const faction of factions) {
@@ -23,8 +27,6 @@ export async function main(ns: NS): Promise<void> {
 			}
 		}
 	}
-	ns.tprint(auglist.toString());
-	//below here doesn't work for some reason
 	let sortedlist = [];
 	while (auglist.length > 0) {
 		let highdex = 0;
@@ -35,21 +37,13 @@ export async function main(ns: NS): Promise<void> {
 				highdex = i;
 			}
 		}
-		sortedlist.push(auglist[highdex]);
-		auglist.splice(highdex);
+		sortedlist.push(...auglist.splice(highdex, 1));
 	}
 	if (gangs && ns.gang.inGang()) {
 		sortedlist = sortedlist.filter(aug => !ns.singularity.getAugmentationsFromFaction(ns.gang.getGangInformation().faction).includes(aug));
-		let uniquefacts = [] as Array<string>;
-		for (const aug of sortedlist) {
-			let augfacts = ns.singularity.getAugmentationFactions(aug);
-			for (const fac of augfacts) {
-				if (!uniquefacts.includes(fac)) { uniquefacts.push(fac); }
-			}
-		}
-		ns.tprint(uniquefacts.toString());
+		for (const aug of sortedlist) { ns.tprint(aug + ": " + ns.singularity.getAugmentationFactions(aug).toString()); }
 	}
-	ns.tprint(sortedlist.toString());
+	else { for (const aug of sortedlist) { ns.tprint(aug + ": " + ns.singularity.getAugmentationFactions(aug).toString()); } }
 	//TODO:
 	//check against a list of factions ordered early -> late
 	//work for each in turn until all augs are acquired
