6c00b28abefc9f4acfa0d2305d7234d86fcc6fb9
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-12-05 06:02:27 +1030

    further setup tweaks

--- setup.ts
+++ setup.ts
@@ -10,7 +10,6 @@ export async function main(ns: NS): Promise<void> {
 	await ns.sleep(300000);
 	const progs = ["BruteSSH.exe", "FTPCrack.exe"];
 	const dwprogs = ["relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"];
-	const exprogs = ["AutoLink.exe", "DeepscanV1.exe", "ServerProfiler.exe", "DeepscanV2.exe"];
 	for (const prog of progs) {
 		if (!ns.fileExists(prog, "home")) {
 			while (!ns.singularity.createProgram(prog, focus)) {
@@ -20,13 +19,28 @@ export async function main(ns: NS): Promise<void> {
 			while (!ns.fileExists(prog, "home")) { await ns.sleep(10000); }
 		}
 	}
-	while (!ns.singularity.purchaseTor()) { await ns.sleep(10000); }
-	for (const dwprog of dwprogs) { if (!ns.fileExists(dwprog, "home")) { while (!ns.singularity.purchaseProgram(dwprog)) { await ns.sleep(10000); } } }
+	while (!ns.singularity.purchaseTor()) {
+		if (ns.singularity.getCrimeChance("Homicide") > 0.5) { await ns.sleep(ns.singularity.commitCrime("Homicide", focus)); }
+		else { await ns.sleep(ns.singularity.commitCrime("Mug", focus)); }
+	}
+	ns.singularity.stopAction();
+	for (const dwprog of dwprogs) {
+		if (!ns.fileExists(dwprog, "home")) {
+			while (!ns.singularity.purchaseProgram(dwprog)) {
+				if (ns.singularity.getCrimeChance("Homicide") > 0.5) { await ns.sleep(ns.singularity.commitCrime("Homicide", focus)); }
+				else { await ns.sleep(ns.singularity.commitCrime("Mug", focus)); }
+			}
+		}
+	}
+	ns.singularity.stopAction();
+	/* for now, find a new home for this
+	const exprogs = ["AutoLink.exe", "DeepscanV1.exe", "ServerProfiler.exe", "DeepscanV2.exe"];
 	for (const exprog of exprogs) {
 		if (!ns.fileExists(exprog, "home")) {
 			while (!ns.singularity.createProgram(exprog, focus)) { await ns.sleep(10000); }
 			while (!ns.fileExists(exprog, "home")) { await ns.sleep(10000); }
 		}
 	}
+	*/
 	ns.run("factionfarmer.js");
 }
