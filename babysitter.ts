ee232d000ffcb959fc4ccbf155f0ae688196f3a5
thefirstpaige <47806856+TheFirstPaige1@users.noreply.github.com>
2023-12-24 10:46:30 +1030

    expanding the babysitter

--- babysitter.ts
+++ babysitter.ts
@@ -2,11 +2,21 @@ import { NS } from "@ns";
 import { quietTheBabblingThrong } from "./bitlib";
 export async function main(ns: NS): Promise<void> {
 	quietTheBabblingThrong(ns);
+	const combatstats = ["str", "def", "dex", "agi"];
 	while (true) {
-		if (ns.hacknet.numHashes() > Math.trunc(ns.hacknet.hashCapacity() * 0.75)) {
-			while (ns.hacknet.numHashes() > Math.trunc(ns.hacknet.hashCapacity() * 0.5)) {
-				ns.hacknet.spendHashes("Sell for Money");
+		let work = ns.singularity.getCurrentWork();
+		if (work.type == "COMPANY") { while (ns.hacknet.spendHashes("Company Favor", work.companyName)) { await ns.sleep(1); } }
+		else if (work.type == "CLASS") {
+			if (combatstats.includes(work.classType)) { while (ns.hacknet.spendHashes("Improve Gym Training")) { await ns.sleep(1); } }
+			else { while (ns.hacknet.spendHashes("Improve Studying")) { await ns.sleep(1); } }
+		}
+		else if (ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("ecorp")) {
+			if (ns.hacknet.hashCost("Increase Maximum Money") > ns.hacknet.hashCost("Reduce Minimum Security")) {
+				while (ns.hacknet.spendHashes("Reduce Minimum Security", "ecorp")) { await ns.sleep(1); }
+			} else {
+				while (ns.hacknet.spendHashes("Increase Maximum Money", "ecorp")) { await ns.sleep(1); }
 			}
-		} else { await ns.sleep(1000); }
+		}
+		await ns.sleep(10000);
 	}
 }
